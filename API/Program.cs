741d46ce7fc73c4b2457f467520e9ffe05a7b077
Sauce77 <sauceortediego505@gmail.com>
2025-09-05 09:48:09 -0600

    crear data dbcontext

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AppDbContext<AppDbContext>( opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+});
 
 var app = builder.Build();
 
