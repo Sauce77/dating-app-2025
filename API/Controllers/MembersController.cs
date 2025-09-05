9cbd47ae1aac317e2e7b94356ac63c2230fa7966
Sauce77 <sauceortediego505@gmail.com>
2025-09-05 15:48:20 -0600

    agregar cliente http a spa

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using API.Data;
 using API.Entities;
 
@@ -13,18 +14,18 @@ public class MembersController(AppDbContext context) : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
     {
-        var members = context.Users.ToList();
+        var members = context.Users.ToListAsync();
 
-        return members;
+        return Ok(members);
     }
 
     [HttpGet("{id}")] // localhost:5201/api/member/bob-id
     public async Task<ActionResult<AppUser>> GetMember(string id)
     {
-        var member = await context.Users.Find(id);
+        var member = await context.Users.FindAsync(id);
 
         if (member == null) return NotFound();
 
-        return member;
+        return Ok(member);
     }
 }
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,7 +8,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
-  protected title2 = "vous etes tres drole";
+export class App implements OnInit{
+  private http = inject(HttpClient);
+
+  protected readonly title = signal('Dating App');
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5201/api/members")
+  }
 }
