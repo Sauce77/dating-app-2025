ac7eda67456fc99c3aaf066eddf1ffb5471d599a
Sauce77 <sauceortediego505@gmail.com>
2025-09-22 08:57:56 -0600

    login post

--- client/src/app/layout/nav/nav.ts
+++ client/src/app/layout/nav/nav.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/service/account-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  protected creds: any = {};
+  private accountService = inject(AccountService);
 
+  login(): void{
+   this.accountService.login(this.creds).subscribe({
+    next: response => console.log(JSON.stringify(response)),
+    error: error => alert(error.message)
+   })
+  }
 }
