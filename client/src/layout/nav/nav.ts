b557310e8ab23837b8f24837e90c19096c0358a2
Sauce77 <sauceortediego505@gmail.com>
2025-10-01 07:56:35 -0600

    se anadio toast service

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/service/account-service';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,11 +12,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 export class Nav {
   protected creds: any = {};
   protected accountService = inject(AccountService);
+  private router = inject(Router)
 
   login(): void{
    this.accountService.login(this.creds).subscribe({
     next: response => {
-      console.log(JSON.stringify(response)),
+      this.router.navigateByUrl("/members")
       this.creds = {};
     },
     error: error => alert(error.message)
