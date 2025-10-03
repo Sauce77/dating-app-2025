0b3d050b166ea2e826257bd9fc7a4b1c4a2d1dd8
Sauce77 <sauceortediego505@gmail.com>
2025-10-03 07:43:08 -0600

    se inserto init-service

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { InitService } from '../core/service/init-service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +12,26 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
+    provideAppInitializer(async() => {
+      const initService = inject(InitService);
+
+      return new Promise<void>((resolve) =>{
+        setTimeout(async () =>{
+            try{
+              return lastValueFrom(initService.init());
+            }finally{
+              const splash = document.getElementById("initial-splash");
+
+              if (splash){
+                splash.remove();
+
+                resolve();
+              }
+            }
+        }, 500);
+      })
+
+      
+    })
   ]
 };
