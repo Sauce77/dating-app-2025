2d3d46f3fb66ab50427a85e3197cdb7a4c232d7e
Sauce77 <sauceortediego505@gmail.com>
2025-10-15 08:13:15 -0600

    seedUsers completado

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -26,10 +26,10 @@ public class Seed
             return;
         }
 
-        using var hmac = new HMACSHA512();
-
         foreach (var seedUser in seedUsers)
         {
+            using var hmac = new HMACSHA512();
+            
             var user = new AppUser
             {
                 Id = seedUser.Id,
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,11 @@
+using System.Text;
 using API.Data;
-using API.Services;
 using API.Interfaces;
 using API.Middlewares;
-using Microsoft.EntityFrameworkCore;
+using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens; // For TokenValidationParameters and SymmetricSecurityKey
-using System.Text; // For Encoding
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,5 +40,19 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localho
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Migration process failed!");
+}
 
 app.Run();
