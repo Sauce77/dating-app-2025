9a9470726a2537e371ed4bb4258d98bec02395bb
Sauce77 <sauceortediego505@gmail.com>
2025-10-16 07:44:25 -0600

    esqueleto repository pattern

---
+++ API/Data/MembersRepository.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Interfaces;
+
+namespace API.Data;
+
+public class MembersRepository : IMembersRepository
+{
+    public Task<Member?> GetMemberAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Member>> GetMembersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Photo>> GetPhotosAsync(string memberId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Member member)
+    {
+        throw new NotImplementedException();
+    }
+}
