8bb3a742491f964708f52c6a3bd65a46b6ac8adf
Sauce77 <sauceortediego505@gmail.com>
2025-10-28 07:51:18 -0600

    insertar router outlet

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { authGuard } from '../core/guard/auth-guard';
 import { TestErrors } from '../features/test-errors/test-errors';
 import { NotFound } from '../shared/errors/not-found/not-found';
 import { ServerError } from '../shared/errors/server-error/server-error';
+import { Profile } from '../features/members/profile/profile'
+import { Photos } from '../features/members/photos/photos'
 
 export const routes: Routes = [
     {path: "", component: Home },
@@ -16,7 +18,14 @@ export const routes: Routes = [
         canActivate: [authGuard],
         children: [
             {path: "members", component: MemberList, canActivate: [authGuard] },
-            {path: "members/:id", component: MemberDetail },
+            {path: "members/:id", component: MemberDetail,
+                children: [
+                    { path: "", redirectTo: "profile", pathMatch: "full" },
+                    { path: "profile", component: Profile, title: "Profile" },
+                    { path: "photos", component: Photos, title: "Photos" },
+                    { path: "messages", component: Messages, title: "Messages" },
+                ]
+             },
             {path: "list", component: Lists },
             {path: "messages", component: Messages },
         ]
