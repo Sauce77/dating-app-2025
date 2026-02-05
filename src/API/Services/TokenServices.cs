c08cc877d820bcdf72e796e9a8981470ad34c829
Sauce77 <sauceortediego505@gmail.com>
2026-02-05 07:49:59 -0600

    Parcial04 - Swagger

--- src/API/Services/TokenServices.cs
+++ src/API/Services/TokenServices.cs
@@ -1,12 +1,9 @@
-using System;
-using API.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
-
-using Microsoft.IdentityModel.Tokens; // For SymmetricSecurityKey, SigningCredentials, SecurityTokenDescriptor
-using System.Security.Claims; // For Claim, ClaimsIdentity, ClaimTypes
-using System.Text; // For Encoding
-using System.IdentityModel.Tokens.Jwt; // For JwtSecurityTokenHandler
-
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services;
 
@@ -14,29 +11,27 @@ public class TokenService(IConfiguration configuration) : ITokenService
 {
     public string CreateToken(AppUser user)
     {
-        var tokenKey = configuration["TokenKey"] ?? throw new ArgumentNullException("TokenKey");
+        var tokenKey = configuration["TokenKey"] ?? throw new ArgumentNullException("Cannot get the token key");
+        if (tokenKey.Length < 64)
+        {
+            throw new ArgumentException("The token key must be >= 64 chars");
+        }
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
-
         var claims = new List<Claim>
         {
             new(ClaimTypes.Email, user.Email),
-            new(ClaimTypes.NameIdentifier, user.Id),
+            new(ClaimTypes.NameIdentifier, user.Id)
         };
-
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
         var tokenDescription = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddDays(7),
             SigningCredentials = creds
         };
-
         var tokenHandler = new JwtSecurityTokenHandler();
-
         var token = tokenHandler.CreateToken(tokenDescription);
 
         return tokenHandler.WriteToken(token);
     }
-
 }
