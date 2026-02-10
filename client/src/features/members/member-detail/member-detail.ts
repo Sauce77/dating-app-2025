1be723858101c0118ce778e50660bc6c16f5815c
Sauce77 <sauceortediego505@gmail.com>
2026-02-10 00:02:02 -0600

    Parcial04 - Added Edit Button

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,8 +1,10 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Member } from '../../../types/member';
 import { filter, Observable } from 'rxjs';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { AccountService } from '../../../core/service/account-service';
+import { MembersService } from '../../../core/service/members-service';
 
 @Component({
   selector: 'app-member-detail',
@@ -13,8 +15,15 @@ import { AgePipe } from '../../../core/pipes/age-pipe';
 export class MemberDetail implements OnInit {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
+  private accountService = inject(AccountService);
+
+  protected membersService = inject(MembersService);
   protected member = signal(<Member | undefined>(undefined))
   protected title = signal<string|undefined>("Profile");
+  protected isCurrentUser = computed(() => {
+    return this.accountService.currentUser()?.id === this.route.snapshot.paramMap.get("id");
+  })
+    
 
   ngOnInit(): void {
     this.route.data.subscribe({
