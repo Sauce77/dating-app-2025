db45ca618e25e2f4b184fd391f28a4b9dc8cdbb9
Sauce77 <sauceortediego505@gmail.com>
2026-02-19 07:35:31 -0600

    Parcial04 - Image Uplaod Component

--- client/src/features/member-photos/member-photos.ts
+++ client/src/features/member-photos/member-photos.ts
@@ -1,25 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { MembersService } from '../../core/service/members-service';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../../types/member';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { ImageUpload } from '../../shared/image-upload/image-upload';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, ImageUpload],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
-export class MemberPhotos {
-  private membersService = inject(MembersService);
+export class MemberPhotos implements OnInit{
+  protected membersService = inject(MembersService);
   private route = inject(ActivatedRoute);
-  protected photos$?: Observable<Photo[]>;
+  protected photos = signal<Photo[]>([]);
+  protected loading = signal(false);
 
-  constructor() {
+  ngOnInit(): void {
     const memberId = this.route.parent?.snapshot.paramMap.get("id");
     if (memberId) {
-      this.photos$ = this.membersService.getPhotos(memberId);
+      this.membersService.getPhotos(memberId).subscribe({
+        next: photos => this.photos.set(photos)
+      });
     }
   }
 
@@ -27,5 +31,20 @@ export class MemberPhotos {
     return Array.from({ length: 0 }, (_, i) => ({
       url: "./user.png"
     }));
+  }
+
+  onUploadImage(file: File){
+    this.loading.set(true);
+    this.membersService.uploadPhoto(file).subscribe({
+      next: photo => {
+        this.membersService.editMode.set(false);
+        this.loading.set(false);
+        this.photos.update(photos => [...photos, photo]);
+      },
+      error: error => {
+        console.log("Erreur pendant le chargement de l'image");
+        this.loading.set(false);
+      }
+    })
   }
 }
