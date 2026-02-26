783cb66eb8c3bc0ae84370c5d1a52f683bbb80e5
Sauce77 <sauceortediego505@gmail.com>
2026-02-26 07:41:53 -0600

    Parcial05 - register form validators

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit, output } from '@angular/core';
 import { RegisterCreds } from '../../../types/users';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AccountService } from '../../../core/service/account-service';
 import { JsonPipe } from '@angular/common';
 
@@ -15,6 +15,8 @@ export class Register implements OnInit {
   protected creds = {} as RegisterCreds;
   protected registerForm: FormGroup = new FormGroup({});
   cancelRegister = output<boolean>();
+
+  private readonly EMAIL_REGEX = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
   
   ngOnInit(): void {
     this.initializeForm();
@@ -22,10 +24,22 @@ export class Register implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      email: new FormControl(),
-      displayName: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl()
+      email: new FormControl('name@email.com', 
+        [Validators.required, Validators.pattern(this.EMAIL_REGEX)]
+      ),
+      displayName: new FormControl('', 
+        [Validators.required]
+      ),
+      password: new FormControl('', 
+        [
+          Validators.required, 
+          Validators.minLength(4), 
+          Validators.maxLength(10)
+        ]
+      ),
+      confirmPassword: new FormControl('',
+        [Validators.required]
+      )
     })
   }
 
