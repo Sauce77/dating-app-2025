a885e87dadba3f0d16846fab096d780c1da750e8
Sauce77 <sauceortediego505@gmail.com>
2026-03-05 01:18:26 -0600

    Paricial05 - Add Form Steps

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit, output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Component, inject, OnInit, output, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../../../core/service/account-service';
 import { RegisterCreds } from '../../../types/users';
 import { JsonPipe } from '@angular/common';
@@ -11,32 +11,34 @@ import { TextInput } from '../../../shared/input-text/input-text';
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register implements OnInit {
+export class Register {
   private accountService = inject(AccountService);
+  private fb = inject(FormBuilder)
   protected creds = {} as RegisterCreds;
-  protected registerForm: FormGroup = new FormGroup({});
+  protected credentialsForm: FormGroup;
+  protected profileForm: FormGroup;
+  protected currentStep = signal(1);
   cancelRegister = output<boolean>();
 
   private readonly EMAIL_REGEX = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
 
-  ngOnInit(): void {
-    this.initializeForm();
-  }
-
-  initializeForm() {
-    this.registerForm = new FormGroup({
-      email: new FormControl('',
-        [Validators.required, Validators.pattern(this.EMAIL_REGEX)]),
-      displayName: new FormControl('',
-        [Validators.required]),
-      password: new FormControl('',
-        [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('',
-        [Validators.required, this.matchValues('password')])
+  constructor() {
+    this.credentialsForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(this.EMAIL_REGEX)]],
+      displayName: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     });
 
-    this.registerForm.controls['password'].valueChanges.subscribe(() => {
-      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    this.profileForm = this.fb.group({
+      gender: ['', [Validators.required]],
+      birthDay: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+    })
+
+    this.credentialsForm.controls['password'].valueChanges.subscribe(() => {
+      this.credentialsForm.controls['confirmPassword'].updateValueAndValidity();
     });
   }
 
@@ -49,9 +51,27 @@ export class Register implements OnInit {
     }
   }
 
+  nextStep() {
+    if(this.credentialsForm.valid) {
+      this.currentStep.update(prevStep => prevStep + 1);
+    }
+  }
+
+  prevStep() {
+    this.currentStep.update(prevStep => prevStep - 1);
+  }
+
   register(): void {
     console.group("REGISTER");
-    console.log(this.registerForm.value);
+    console.log(this.credentialsForm.value);
+    console.log(this.currentStep());
+    
+
+    if(this.credentialsForm.valid && this.profileForm.valid) {
+      const formData = { ...this.credentialsForm.value, ...this.profileForm.value };
+      console.log(formData);
+    }
+
     console.groupEnd();
     // this.accountService.register(this.creds).subscribe({
     //   next: response => {
