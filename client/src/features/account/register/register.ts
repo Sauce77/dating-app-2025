78706abe0303e879be5be047b0da97d58bf771fb
Sauce77 <sauceortediego505@gmail.com>
2026-03-05 07:41:15 -0600

    Parcial05 - Add form steps

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -61,6 +61,12 @@ export class Register {
     this.currentStep.update(prevStep => prevStep - 1);
   }
 
+  getMaxDate() {
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - 18);
+    return today.toISOString().split('T')[0];
+  }
+
   register(): void {
     console.group("REGISTER");
     console.log(this.credentialsForm.value);
