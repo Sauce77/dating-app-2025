4d904762dd366d6907e6d745968727baa6ef9900
Sauce77 <sauceortediego505@gmail.com>
2026-03-26 07:38:05 -0600

    Parcial05 - Fix modal and restore cache

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpInterceptorFn, HttpParams } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { BusyService } from '../service/busy-service';
 import { delay, finalize, of, tap } from 'rxjs';
@@ -8,11 +8,21 @@ const cache = new Map<string, HttpEvent<unknown>>();
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService);
 
+  const generateCache = (url: string, params: HttpParams): string => {
+    const paramsString = params.keys()
+      .map(key => `${key}=${params.get(key)}`)
+      .join(`&`);
+
+      return paramsString ? `${url}?${paramsString}` : url;
+  }
+
+  const cacheKey = generateCache(req.url, req.params);
+
   if (req.method === 'GET') {
-    // const cachedResponse = cache.get(req.url);
-    // if (cachedResponse) {
-    //   return of(cachedResponse);
-    // }
+    const cachedResponse = cache.get(req.url);
+    if (cachedResponse) {
+      return of(cachedResponse);
+    }
   }
 
   busyService.busy();
@@ -20,7 +30,7 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(
     delay(500),
     tap(response => {
-      cache.set(req.url, response)
+      cache.set(cacheKey, response)
     }),
     finalize(() => {
       busyService.idle();
