c0d3c351e699844b844735761c1915434e129b30
Sauce77 <sauceortediego505@gmail.com>
2026-04-09 07:33:14 -0600

    Parcial06 - Add initial migration for Message

--- src/API/Data/AppDbContext.cs
+++ src/API/Data/AppDbContext.cs
@@ -24,6 +24,24 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
             .HasForeignKey(s => s.TargetMemberId)
             .OnDelete(DeleteBehavior.Cascade);
 
+        modelBuilder.Entity<Message>()
+            .HasOne(m => m.Recipient)
+            .WithMany(mr => mr.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Message>()
+            .HasOne(m => m.Sender)
+            .WithMany(mr => mr.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Message>()
+            .Property(m => m.RecipientDeleted)
+            .HasDefaultValue(false);
+
+        modelBuilder.Entity<Message>()
+            .Property(m => m.SenderDeleted)
+            .HasDefaultValue(false);
+
         var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
             v => v.ToUniversalTime(),
             v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
