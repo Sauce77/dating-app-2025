7a0087581b8634d348b749bdebb34a0c6ef90fc8
Sauce77 <sauceortediego505@gmail.com>
2026-04-13 09:10:56 -0600

    Parcial06 - Add messages controller

---
+++ src/API/DTOs/MessageRequest.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs;
+public class MessageRequest
+{
+    public required string RecipientId { get; set; }
+    public required string Content { get; set; }
+}
--- src/API/Interfaces/IMessagesRepository.cs
+++ src/API/Interfaces/IMessagesRepository.cs
@@ -1,23 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.DTOs;
 using API.Entities;
 using API.Helpers;
-using API.DTOs;
 
 namespace API.Interfaces;
 
 public interface IMessagesRepository
 {
-    void Add(Message message);
-    void Delete(Message message);
-
-    Task<Message?> Get(string messageId);
-
-    Task<PaginationResult<MessageResponse>> GetForMember();
-
-    Task<IReadOnlyList<Member>> GetThread(string currentMemberId, string recipientId);
-
-    Task<bool> SaveAllAsync();
+   void Add(Message message);
+   void Delete(Message message);
+   Task<Message?> Get(string messageId);
+   Task<PaginationResult<MessageResponse>> GetForMember();
+   Task<IReadOnlyList<MessageResponse>> GetThread(string currentMemberId, string recipientId);
+   Task<bool> SaveAllAsync(); 
 }
