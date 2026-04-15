9b8e644533b00e0dde8063a450ab8df29181b0c6
Sauce77 <sauceortediego505@gmail.com>
2026-04-15 07:32:17 -0600

    Parcial06 - Add get message thread

--- src/API/Controllers/MessagesController.cs
+++ src/API/Controllers/MessagesController.cs
@@ -48,4 +48,11 @@ public class MessagesController(
 
         return await messagesRepository.GetForMember(messageParams);
     }
+
+    [HttpGet("thread/{recipientId}")]
+    public async Task<ActionResult<IReadOnlyList<MessageResponse>>> GetThread(string recipientId)
+    {
+        return Ok(await messagesRepository.GetThread(User.GetMemberId(), recipientId));
+    }
+
 }
