2af9095dbd64783bf9cf13aad89a8771d137131a
Sauce77 <sauceortediego505@gmail.com>
2026-04-16 07:14:25 -0600

    Parcial06 - Add get messages SPA

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MessageService } from '../../core/service/message-service';
+import { PaginationResult } from '../../types/paginationMetadata';
+import { Message } from '../../types/message';
 
 @Component({
   selector: 'app-messages',
@@ -7,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './messages.css'
 })
 export class Messages {
+  private messagesService = inject(MessageService);
+  protected container = 'inbox';
+  protected pageNumber = 1;
+  protected pageSize = 10;
+  protected paginatedMessage = signal<PaginationResult<Message> | null>(null);
 
+  ngOnInit(): void {
+    throw new Error("Method not implemented.")
+  }
+  
+  loadMessage() {
+    this.messagesService.getMessages(this.container, this.pageNumber, this.pageSize).suscribe({
+      next: 
+    });
+  }
 }
