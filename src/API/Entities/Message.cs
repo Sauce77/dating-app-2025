19e42f7b6d0c57bbf67e0e8d7b3e7b164a4fd214
Sauce77 <sauceortediego505@gmail.com>
2026-05-13 20:41:40 -0600

    Parcial06 - FIx SenderDeleted intialized as true

--- src/API/Entities/Message.cs
+++ src/API/Entities/Message.cs
@@ -1,27 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.Entities;
+
 public class Message
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string Content { get; set; }
-    
     public DateTime? DateRead { get; set; }
-
     public DateTime MessageSent { get; set; } = DateTime.UtcNow;
-
     public bool SenderDeleted { get; set; }
-
-    public bool RecipientDeleted { get; set; }
-
     public required string SenderId { get; set; }
-
+    public bool RecipientDeleted { get; set; }
     public required string RecipientId { get; set; }
 
-    // Naviigation properties
+    // Navigation properties
     public Member Sender { get; set; } = null!;
-    public Member Recipient { get; set;} = null!;
+    public Member Recipient { get; set; } = null!;
+
 }
