1fd5e56b126da8fcc5043fe590adce52c7785f45
Sauce77 <sauceortediego505@gmail.com>
2026-05-18 07:48:03 -0600

    Parcial06 - Add presenceHub to client

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: "https://localhost:5201/api/",
+    hubUrl: "https://localhost:5201/api/",
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: "http://localhost:5201/api/",
+    hubUrl: "https://localhost:5201/api/",
 };
